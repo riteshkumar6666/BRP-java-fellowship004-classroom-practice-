c7ece7c223e54b943b1f7dd207da6c460e46724c
Ritesh Sagar <riteshkumar625.rk@gmail.com>
2021-10-05 08:15:42 +0100

    MATRIX PROGRAM

---
+++ src/SumOfTwoMatrix.java
@@ -0,0 +1,64 @@
+//***BRP-java fellowship_004***
+//Q.wap to sum of two matrix program in java
+import java.util.Scanner;
+public class SumOfTwoMatrix {
+    public static void main(String[] args) {
+        int a[][]=new int[2][2];
+        int b[][]=new int[2][2];
+        int c[][]=new int[2][2];
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("ENTER FIRST MATRIX DATA");
+        for (int i=0;i<2;i++)  //rows
+        {
+            for (int j=0;j<2;j++) //column
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+
+        System.out.println("ENTER SECOND MATRIX DATA");
+        for (int i=0;i<2;i++)  //rows
+        {
+            for (int j=0;j<2;j++) //column
+            {
+                b[i][j]=sc.nextInt();
+            }
+        }
+
+        System.out.println("FIRST MATRIX \n");
+        for (int i=0;i<2;i++)  //rows
+        {
+            for (int j=0;j<2;j++) //column
+            {
+                System.out.println(a[i][j]+" ");
+            }
+            System.out.println("\n");
+        }
+
+        System.out.println("SECOND MATRIX \n");
+        for (int i=0;i<2;i++)  //rows
+        {
+            for (int j=0;j<2;j++) //column
+            {
+                System.out.println(b[i][j]+" ");
+            }
+            System.out.println("\n");
+        }
+
+        System.out.println("ADDITION OF MATRIX \n");
+        for (int i=0;i<2;i++)  //rows
+        {
+            for (int j=0;j<2;j++) //column
+            {
+                c[i][j]=a[i][j]+b[i][j];
+
+                System.out.println(c[i][j]+ " ");
+            }
+            System.out.println("\n");
+        }
+
+    }
+}
+
